4a6f07fd79f1bb05f3c1116e78b4dbfdc513459a
Nick Marucci <npm1514@gmail.com>
2016-02-14 19:24:40 -0700

    change port number

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ var express   = require('express'),
     console.log("Connected to mongoDB");
   });
 
-  app.listen(9000, function(){
+  app.listen(80, function(){
     console.log("listening to 80");
   });
